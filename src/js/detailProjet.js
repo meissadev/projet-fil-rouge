26bdf0ae670f708b39da250f2d7794c6b32d9a2f
Mame Yacine Ndiaye <nyacine183@gmail.com>
2026-04-09 09:42:56 +0000

    restauration detailProjet

--- src/js/detailProjet.js
+++ src/js/detailProjet.js
@@ -1,1 +1,3 @@
-/* ======================= detailProjet.js - Affichage des détails d'un projet (Mame Yacine) ======================= */bonjour
+
+
+/* ======================= detailProjet.js - Affichage des détails d'un projet (Mame Yacine) ======================= */
